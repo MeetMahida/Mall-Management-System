87ea19093a5ee58bb6986c82c921fc39cf062ec1
MeetMahida <103932225+MeetMahida@users.noreply.github.com>
2022-04-29 09:11:57 +0530

    Delete billing directory

--- billing/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Customer
-
-class GenerateBillForm(forms.ModelForm):
-  class Meta:
-    model = Customer
-    fields = ('product', 'quantity', 'fname', 'lname', 'date')
